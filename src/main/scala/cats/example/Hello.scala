6a1b35bcbe5b400a7a80010be9b8893892186c0f
twr143 <twr143@gmail.com>
2019-08-05 21:11:56 +0300

    MapN revision, 2

--- src/main/scala/cats/example/Hello.scala
+++ src/main/scala/cats/example/Hello.scala
@@ -1,7 +1,13 @@
 package cats.example
-import cats._, cats.instances.all._
+import cats._
+import cats.instances.all._
 import cats.syntax.functor._
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.util.Success
+
 object Hello extends App {
+
   val len: String => Int = _.length
 
   val result = Functor[List].map(List("scala", "cats"))(len)
@@ -12,10 +18,15 @@ object Hello extends App {
 
   println(s"pairs: $result2")
 
-  val lst1 = List(1,2,3)
-  val lst2 = List(4,5,6)
+  val lst1 = List(1, 2, 3)
+
+  val lst2 = List(4, 5, 6)
   import cats.implicits._
-  println(s"++: "+ (lst1++lst2))
-  println(s"|@|: "+ (lst1,lst2).mapN((a,b)=>(a,b)))
+
+  println(s"++: " + (lst1 ++ lst2))
+
+  println(s"|@|: " + (lst1, lst2).mapN((a, b) => (a + b)))
+
+  (Future(1), Future(2)).mapN((a, b) => a + b).andThen { case Success(x) => println(x) }
 }
 
